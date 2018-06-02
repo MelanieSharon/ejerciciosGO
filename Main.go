4bed9dbfdccce7fa29ae4fe908535061f83f3bc2
MelanieSharon <mell.250697@gmail.com>
2018-06-02 14:11:09 -0500

    Reutilizar funcion

--- Main.go
+++ Main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	conditional(1)
 	fmt.Println(conditionalIF(21))
+	fmt.Println(conditionalDuo(0))
+	fmt.Println(respuestaCondition(conditionalDuo(0)))
 }
 
 func conditional(number int) {
@@ -34,4 +36,32 @@ func conditionalIF(number int) string {
 		name = "Viejo"
 	}
 	return name
+}
+
+func conditionalDuo(number int) float64 {
+	var rpt = 0.0
+
+	switch number {
+	case 0:
+		rpt = 5 * 0.20
+	case 1:
+		rpt = 5.20 * 0.15
+	case 2:
+		rpt = 6.0 * 8.0
+	}
+
+	return rpt
+}
+
+func respuestaCondition(rpt float64) string {
+	var result = ""
+	if rpt <= 2 {
+		result = "saldo completo"
+	} else if rpt > 2 && rpt <= 4 {
+		result = "saldo incompleto"
+	} else {
+		result = "Error sistema"
+	}
+
+	return result
 }
